537aba633af71ab388720286f4c76c1c9fcb682d
Lucas Pinho <lucas.pinho@semantix.com.br>
2020-10-09 17:20:37 -0300

    Internationalization de APIs

--- src/main/java/com/claro/rest/webservices/test/helloworld/HelloWorldController.java
+++ src/main/java/com/claro/rest/webservices/test/helloworld/HelloWorldController.java
@@ -1,13 +1,21 @@
 package com.claro.rest.webservices.test.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	@Autowired
+	private MessageSource messageSource;
 	
 	// método para retornar 'Hello World'
 	// GET
@@ -39,6 +47,12 @@ public class HelloWorldController {
 		
 		return new HelloWorldBean(String.format("Hello World, %s", name));
 				
+	}
+	
+	@GetMapping(path="/helloworld-internationalized")
+	public String helloWorldInternationalized(@RequestHeader(name="Accept-Language", required=false) Locale locale) {
+		
+		return messageSource.getMessage("good.morning.message", null,  locale);
 	}
 
 }
