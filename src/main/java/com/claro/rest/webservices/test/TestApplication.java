fde552809bc2a0f90b4fd534967fc8185296d864
Lucas Pinho <lucas.pinho@semantix.com.br>
2020-10-08 23:54:46 -0300

    RESTFul API com Spring Boot

---
+++ src/main/java/com/claro/rest/webservices/test/TestApplication.java
@@ -0,0 +1,13 @@
+package com.claro.rest.webservices.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+
+}
