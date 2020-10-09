e793199875d65affd1693a12f18a0614bafdbce4
Lucas Pinho <lucas.pinho@semantix.com.br>
2020-10-09 17:41:54 -0300

    Internationalize API sem precisar enviar locale como parâmetro no header do request utilizando LocaleContextHolder.getLocale()

--- src/main/java/com/claro/rest/webservices/test/helloworld/HelloWorldController.java
+++ src/main/java/com/claro/rest/webservices/test/helloworld/HelloWorldController.java
@@ -1,12 +1,10 @@
 package com.claro.rest.webservices.test.helloworld;
 
-import java.util.Locale;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,9 +48,9 @@ public class HelloWorldController {
 	}
 	
 	@GetMapping(path="/helloworld-internationalized")
-	public String helloWorldInternationalized(@RequestHeader(name="Accept-Language", required=false) Locale locale) {
+	public String helloWorldInternationalized() {
 		
-		return messageSource.getMessage("good.morning.message", null,  locale);
+		return messageSource.getMessage("good.morning.message", null,  LocaleContextHolder.getLocale());
 	}
 
 }
