8b091515d36778555b9dd190a0f1c97ffad560a6
Lucas Pinho <lucas.pinho@semantix.com.br>
2020-10-09 14:43:02 -0300

    Adicionar spring-boot-starter-validation para validação de entradas do Método POST utilizando as anotações @Valid, @Size e @Past

--- src/main/java/com/claro/rest/webservices/test/user/User.java
+++ src/main/java/com/claro/rest/webservices/test/user/User.java
@@ -2,12 +2,17 @@ package com.claro.rest.webservices.test.user;
 
 import java.util.Date;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
 public class User {
 	
 	private Integer id;
 	
+	@Size(min=2)
 	private String name;
 	
+	@Past
 	private Date birthDate;
 	
 	// Construtor (Alt + Shift + S)
--- src/main/java/com/claro/rest/webservices/test/user/UserController.java
+++ src/main/java/com/claro/rest/webservices/test/user/UserController.java
@@ -3,6 +3,8 @@ package com.claro.rest.webservices.test.user;
 import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -52,7 +54,7 @@ public class UserController {
 	// output - CREATED & retornar também o novo usuário criado. Além de retornar também o URI do novo usuário criado.
 	
 	@PostMapping("/users")
-	public ResponseEntity<Object> createUser(@RequestBody User user) {
+	public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
 		
 		User savedUser = service.saveUser(user);
 		
